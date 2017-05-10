b73d29ac3d6fb60f1f630b0741e587d0dba981b2
Burin Choomnuan <agilecreativity@gmail.com>
2017-05-10 18:27:10 -0400

    Add sample screenshot

--- src/main/java/try_virgil/SimpleLib.java
+++ src/main/java/try_virgil/SimpleLib.java
@@ -1,7 +1,7 @@
 package try_virgil;
 
 public class SimpleLib {
-    //
+    // Simple greeting
     public void greeting(String name) {
         System.out.println("Hi there, " + name);
     }
@@ -14,14 +14,19 @@ public class SimpleLib {
         return n1 + n2;
     }
 
+    public int multNumber(int n1, int n2) {
+        return n1 * n2;
+    }
+
     public static void main(String[] args) {
         SimpleLib app = new try_virgil.SimpleLib();
 
         app.greeting("Clojurian");
         app.sayHi();
+
         System.out.println("FYI: add two number(3,4)" + app.addNumber(3,4));
 
-        String home = System.getenv("HOME");
-        System.out.println("HOME:" + home);
+        String userHome = System.getenv("HOME");
+        System.out.println("userHome" + userHome);
     }
 }
